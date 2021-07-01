951ba07563b3e2e88b06b19d5ddb8794bbab6d50
eddiejungle <eddiejung220@gmail.com>
2021-07-01 16:01:46 -0400

    Initial Commit

---
+++ war-challenge/ContentView.swift
@@ -0,0 +1,95 @@
+//
+//  ContentView.swift
+//  war-challenge
+//
+//  Created by Edward Jung on 6/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
+    
+    var body: some View {
+        ZStack{
+
+            Image("background")
+                .ignoresSafeArea()
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                
+                Button(action: {
+                    
+                    //generate a random number between 2 and 14
+                    let playerRand = Int.random(in: 2...14)
+                    
+                    let cpuRand = Int.random(in:2...14)
+                    
+                    //update cards
+                    playerCard = "card"+String(playerRand)
+                    cpuCard = "card"+String(cpuRand)
+                    
+                    //update score
+                    if playerRand > cpuRand{
+                        playerScore += 1
+                    }
+                    else if cpuRand > playerRand{
+                        cpuScore += 1
+                    }
+                    //playerScore += 1
+                    //cpuScore += 1
+                    
+                }, label:{
+                Image("dealbutton")
+
+                })
+                
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player").font(.headline).foregroundColor(.white).padding(.bottom, 10.0)
+                        Text(String(playerScore)).font(.largeTitle).foregroundColor(.white)
+                    }
+                    Spacer()
+                    
+                    VStack{
+                        Text("CPU")
+                            .font(.headline).foregroundColor(.white)
+                            .padding(.bottom, 10.0)
+                        Text(String(cpuScore))
+                            .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/).foregroundColor(.white)
+                            
+                    }
+                    Spacer()
+                    
+                }
+                
+                
+                Spacer()
+                
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Edward Jung on 6/19/21.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
